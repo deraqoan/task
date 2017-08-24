47c64f76d0428cb0c6c8b2a141dcc93b701a693d
huangmeixiu <1065728516@qq.com>
2017-08-24 20:39:26 +0800

    pass

---
+++ lis/主页面/js/icon.js
@@ -0,0 +1,17 @@
+$('#modalExample, #hide, #hideT').click(function() {
+  $('#modalExample').hide();
+  $('#modal-wrapper').hide();
+  $('#modalExample2').hide();
+  $('#modal-wrap').hide();
+});
+
+$('#shown').click(function() {
+  $('#modalExample').show();
+  $('#modal-wrapper').show();
+});
+$('#showed').click(function() {
+  $('#modalExample2').show();
+  $('#modal-wrap').show();
+});
+
+
